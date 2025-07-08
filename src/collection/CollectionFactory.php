471c95bcafa4a5eb8b8bb1435ad83380011f90e7
Doug Wilbourne <dougwilbourne@gmail.com>
2025-07-08 13:50:20 -0400

    changed return type on makeCollection

--- src/collection/CollectionFactory.php
+++ src/collection/CollectionFactory.php
@@ -6,18 +6,19 @@ declare(strict_types=1);
 namespace pvc\struct\collection;
 
 use pvc\interfaces\struct\collection\CollectionFactoryInterface;
+use pvc\interfaces\struct\collection\CollectionInterface;
 
 /**
  * @template ElementType
- * @implements CollectionFactoryInterface<ElementType, Collection>
+ * @implements CollectionFactoryInterface<ElementType, CollectionInterface>
  */
 class CollectionFactory implements CollectionFactoryInterface
 {
     /**
      * @param array<non-negative-int, ElementType> $elements
-     * @return Collection<ElementType>
+     * @return CollectionInterface<ElementType>
      */
-    public function makeCollection(array $elements = []): Collection
+    public function makeCollection(array $elements = [])
     {
         return new Collection($elements);
     }
--- src/collection/CollectionOrderedFactory.php
+++ src/collection/CollectionOrderedFactory.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace pvc\struct\collection;
 
 use pvc\interfaces\struct\collection\CollectionFactoryInterface;
+use pvc\interfaces\struct\collection\CollectionOrderedInterface;
 use pvc\interfaces\struct\collection\IndexedElementInterface;
 
 /**
  * @template ElementType of IndexedElementInterface
- * @implements CollectionFactoryInterface<ElementType, CollectionOrdered>
+ * @implements CollectionFactoryInterface<ElementType, CollectionOrderedInterface>
  */
 class CollectionOrderedFactory implements CollectionFactoryInterface
 {
     /**
      * @param array<non-negative-int, ElementType> $elements
-     * @return CollectionOrdered<ElementType>
+     * @return CollectionOrderedInterface<ElementType>
      */
-    public function makeCollection(array $elements = []): CollectionOrdered
+    public function makeCollection(array $elements = [])
     {
         return new CollectionOrdered($elements);
     }
